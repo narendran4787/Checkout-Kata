5bf28db2b5613004a2e30980ab434dd1520a2f25
Naren <naren87@proton.me>
2024-02-26 23:20:22 +0000

    Adding test project and empty test methods. Defined test cases

---
+++ BrightHRKata.Tests/CheckoutTests.cs
@@ -0,0 +1,42 @@
+using Moq;
+
+namespace BrightHRKata.Tests;
+
+[TestClass]
+public class CheckoutTests
+{
+    
+    [TestMethod]
+    public void GivenItemsScannedIteratively_ProducesCorrectResult_WhenNoRepeatedItems()
+    {
+    }
+    
+    [TestMethod]
+    public void GivenItemsScannedTogetherAsString_ProducesCorrectResult_WhenNoRepeatedItems()
+    {
+    }
+
+    [TestMethod]
+    public void GivenItemsScannedIteratively_ProducesCorrectResult_WhenRepeatedItems()
+    {
+        
+    }
+    
+    [TestMethod]
+    public void GivenItemsScannedTogetherAsAString_ProducesCorrectResult_WhenRepeatedItems()
+    {
+        
+    }
+    
+    [TestMethod]
+    public void GivenItemsScannedTogetherAsAString_ProducesCorrectResult_WhenPriceChangeAfterTransactionStarted()
+    {
+        
+    }
+    
+    [TestMethod]
+    public void GivenItemsScannedIteratively_ProducesCorrectResult_WhenPriceChangeAfterTransactionStarted()
+    {
+        
+    }
+}
